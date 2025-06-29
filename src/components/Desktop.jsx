21feaba80d7bb5b1a0d8e26d7e16c2b4632448dc
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-06-29 20:27:30 +0300

    Import Icon and Window components in Desktop
    Added imports for the Icon and Window components in Desktop.jsx to enable their usage within the Desktop component.

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import styles from "./Desktop.module.css";
 import Taskbar from "./Taskbar";
 import StartMenu from "./StartMenu";
+import Icon from "./Icon";
+import Window from "./Window";
 
 function Desktop() {
   const [isStartMenuOpen, setIsStartMenuOpen] = useState(false);
