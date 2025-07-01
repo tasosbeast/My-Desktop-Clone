7ae73a42adcb704f91cf2a9ebf5b2d1bff434f35
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-07-01 17:46:52 +0300

    Add app grid to StartMenu with interactive icons
    Replaces placeholder content in StartMenu with a grid of app icons using the new menuGrid style. Clicking an icon triggers onOpenApp and closes the menu. Updates StartMenu to accept onOpenApp and onCloseMenu props for interactivity.

--- src/components/StartMenu.jsx
+++ src/components/StartMenu.jsx
@@ -1,12 +1,50 @@
 // src/components/StartMenu.jsx
 import React from "react";
 import styles from "./StartMenu.module.css"; // Use module CSS
+import Icon from "./Icon";
 
-function StartMenu() {
+function StartMenu({ onOpenApp, onCloseMenu }) {
+  const startMenuItems = [
+    {
+      id: "vscode",
+      name: "VS Code",
+      image: "/icons8-visual-studio-code.svg",
+      type: "app",
+      content: "VS Code Editor",
+    },
+    {
+      id: "chrome",
+      name: "Google Chrome",
+      image: "/icons8-google-chrome.svg",
+      type: "app",
+      content: "Web Browser",
+    },
+    {
+      id: "github",
+      name: "Github Desktop",
+      image: "/icons8-github.svg",
+      type: "app",
+      content: "github desktop app",
+    },
+  ];
+
+  const handleAppClick = (item) => {
+    onOpenApp(item.id, item.name, item.content, item.image);
+    onCloseMenu();
+  };
   return (
     <div className={styles.startMenu}>
-      <h3>Start Menu Content</h3>
-      {/* This is where your start menu items will eventually go */}
+      {/* 4. Add a grid container */}
+      <div className={styles.menuGrid}>
+        {startMenuItems.map((item) => (
+          <Icon
+            key={item.id}
+            name={item.name}
+            image={item.image}
+            onClick={() => handleAppClick(item)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
